50aee83a442c85fb1d201a7185b48df3918158ff
ABIGAIL RODRÍGUEZ MOTA <ar2019-0095@uce.edu.do>
2021-07-15 20:31:55 -0400

    Arreglos del model

--- Data/Models/Article.cs
+++ Data/Models/Article.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,15 +8,24 @@ namespace Data.Models
 {
     public partial class Article
     {
+        [Required]
         public int ArticleId { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        [Required]
         public int? AuthorId { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Content { get; set; }
+        [Required]
         public string Urlarticle { get; set; }
+        [Required]
         public string Urlimagen { get; set; }
         public DateTime? PublishedAt { get; set; }
         public int? Uptake { get; set; }
+        [Required]
         public int? SourceId { get; set; }
         public int? CategoryId { get; set; }
         public int? CountryId { get; set; }
